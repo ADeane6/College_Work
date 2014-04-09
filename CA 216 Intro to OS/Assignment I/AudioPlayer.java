c82af6ddb0207f0a6130f358fbd1d8bf2dae4113
bbbiiittt <bbbiiittt1198@gmail.com>
2014-04-09 16:25:42 +0100

    Steps towards Completion -Consumer setup to play a chunk from buffer -buffer setup to get song info -both insertChunk() and removeChunk() completed

--- CA 216 Intro to OS/Assignment I/AudioPlayer.java
+++ CA 216 Intro to OS/Assignment I/AudioPlayer.java
@@ -1,38 +1,99 @@
 import javax.sound.sampled.*;
 import java.applet.Applet;
-class Buffer
+class BoundedBuffer
 {
-	int nextIn ;
-	int nextOut ;
-	int size ;
-    int occupied ;
-	int ins ;
-	int outs ;
-	int chunks;
+	int nextIn;
+	int nextOut;
+	int size;
+    int occupied;
+	int ins;
+	int outs;
+	int chunkSize;
 	byte [] buffer;
+	int duration;
+	AudioFormat format;
+	DataLine.Info info;
 	bool dataAvailable; //for consumer
 	bool roomAvailable; //for producer
+	BoundedBuffer()
+	{
+		nextIn = 0;
+		nextOut = 0;
+		occupied = 0;
+		ins = 0;
+		outs = 0;
+  }
+	}
 	void getinfo()
 	{
 		AudioInputStream s;
 		s.getAudioInputStream(new File('song.wav');
-		AudioFormat f=s.getFormat();
+		format=s.getFormat();
+		info = new DataLine.Info(SourceDataLine.class, format);
 		
-		System.out.prinln("Audio format: "+f.toString());
+		System.out.prinln("Audio format: "+format.toString());
 		
-		chunks = (int)(f.getChannels()*f.getSampleRate() * f.getSampleSizeInBits()/8);
+		chunks = (int)(format.getChannels()*format.getSampleRate() * format.getSampleSizeInBits()/8);
 		buffer = new byte[chunks*10];
 		size=chunks*10;
+		long frames = audioInputStream.getFrameLength();
+		duration = (int)(frames+0.0) / format.getFrameRate();
+		*/
 	
 	}
 
-}
-class Produce implements runnable
-{
+	boolean over()
+	{
+		return !(out <= duration);
+	}
+
+	void insertChunk(byte[] chunk)
+	{
+		byte[] chunk = new byte[chunkSize];
+		for (int i = nextIn, int k = 0; i <=(chunkSize+nextIn); i++, k++)
+		{
+			buffer[i] = chunk[k];
+		}
+		nextIn += chunkSize;
+		nextIn = nextIn%size;
+		occupied++;
+		ins++;
+	}
 	
+	byte[] removeChunk()
+	{
+		byte[] chunk = new byte[chunkSize];
+		for (int i = nextOut, int k = 0; i <=(chunkSize+nextOut); i++, k++)
+		{
+			chunk[k] = buffer[i];
+		}
+		nextOut += chunkSize;
+		nextOut = nextOut%size;
+		occupied--;
+		outs++;
+		return chunk;
+	}
+
+}
+class Producer implements Runnable
+{
+	BoundedBuffer bBuffer;
+
+	Producer(BoundedBuffer buffer) {
+    bBuffer = buffer;
+	}
+
 	public void run()
 	{
-	
+		try
+		{
+			while(bBufer.roomAvailable)
+		}
+		catch (InterruptedException e)
+		{
+			System.out.println("Bye Bye from Producer");
+			return;
+		}
 	
 	
 	}
@@ -41,9 +102,37 @@ class Produce implements runnable
 
 class Consumer implements Runnable
 {
+	BoundedBuffer bBuffer;
+
+	Consumer(BoundedBuffer buffer) {
+    bBuffer = buffer;
+	}
+
 	public void run()
 	{
-	
+		try
+		{
+
+			SourceDataLine line = (SourceDataLine) AudioSystem.getLine(bBuffer.info);
+			line.open(bBuffer.format);
+			line.start();
+			while(!bBuffer.over())
+			{
+				byte[] audioChunk = bBuffer.removeChunk();
+				line.write(audioChunk, 0, bBuffer.chunkSize);
+
+			}
+			line.drain();
+			line.stop();
+			line.close();
+			System.out.println("Bye Bye from DJ");
+			return;
+		}
+		catch (InterruptedException e)
+		{
+			System.out.println("Bye Bye from DJ");
+			return;
+		}
 	
 	
 	}
@@ -53,9 +142,19 @@ class AudioPlayer
 {
 	public static void main(String[]args)
 	{
-		File f = new File('song.wav');
 		Buffer stream= new Buffer();
 		stream.getinfo();
+		
+
+		Producer reader = new Producer(stream);
+		Consumer dj = new Consumer(stream);
+
+		reader.start();
+		dj.start();
+
+
+
+
 
 	
 	}
